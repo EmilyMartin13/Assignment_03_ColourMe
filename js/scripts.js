9c8148723d4ca6afc28531e48bf36747664a7c73
EmilyMartin13 <em.martin@uleth.ca>
2023-12-06 01:18:01 -0700

    dec 6 1:17 on campus still :(

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,24 @@ console.log(x)
 
 
 var newColor;
+var floral = $("#fresh-cut-flowers");
+var catus = $("#catus");
 
 // Click event for color options
 $('.color-option').click(function () {
     newColor = $(this).data('color');
     console.log(newColor);
-   
+
 });
 
-$(".shape").on("click", function () {
+
+// example:
+// $(".shape").on("click", function () {
+//     $(this).attr('fill', newColor);
+// });
+
+$(".cls-1, .cls-2, .cls-3").on("click", function () {
+    console.log("element worked!");
     $(this).attr('fill', newColor);
 });
 
@@ -20,4 +29,21 @@ $("#lightbulb").click(function () {
     alert('#Pro Tip!Use the paint brush to paint the colouring page. If you make a mistake, thats okay! Just use the eraser to clear colours!');
 });
 
+$("#flowers").on("click", function () {
+    floral.show();
+});
+
+$("#catci").on("click", function () {
+    catus.show();
+});
+
+$("#catci").on("click", function () {
+    floral.hide();
+});
+
+$("#flowers").on("click", function () {
+    catus.hide();
+});
+
+//  $(“body”).css(“cursor”, “cursor” + newColor + ("..//imgs/tip-01.png")
 
